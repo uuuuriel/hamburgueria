e6f2b73702a67b4274b7aaeaa66e27df048004a8
Uriel <Uriel>
2016-02-23 21:50:59 -0300

    Implementação da sugestão e crítica, backup do bd

--- Hamburgueria/WebContent/index.js
+++ Hamburgueria/WebContent/index.js
@@ -7,28 +7,3 @@ $(document).ready(function(){
     $('#sugestaoCritica').load('resources/sugestaocritica.html');
 });
 
-function critica(){
-	bootbox.dialog({
-		message: "<input type='text' class='form-control' id='txtemailCS'/>",
-	  	title: "hue<hr>",
-	  	size: 'small',
-	  	onEscape: function() {},
-	  	buttons: {
-	    	success: {
-	      		label: "Adicionar ao Carrinho",
-	      		className: "btn-success",
-	      		callback: function() {
-	      			var hue = $('txtemailCS').val();
-	        		alert(hue);
-	  	    			}
-	    			},
-	    		danger: {
-		      	label: "Fechar",
-		      	className: "btn-danger",
-		      	callback: function() {
-		       		$(this).fadeOut(1000);
-		      		}
-		    	}
-			}
-		});
-};
