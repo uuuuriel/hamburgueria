8c934f8610aa8e9cfb9be1a7140c93663f31aa27
Uriel Hass <uriel@passecarros.net>
2016-03-18 17:24:30 -0300

    alterações da hamburgueria no trabalho

--- Hamburgueria/src/br/com/hamburgueria/jdbc/JDBCPedidoDAO.java
+++ Hamburgueria/src/br/com/hamburgueria/jdbc/JDBCPedidoDAO.java
@@ -5,11 +5,15 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import br.com.hamburgueria.exception.HamburgueriaException;
 import br.com.hamburgueria.jdbcinterface.PedidoDAO;
+import br.com.hamburgueria.objs.ListaPedido;
 import br.com.hamburgueria.objs.Pedido;
+import br.com.hamburgueria.objs.Usuario;
 
 public class JDBCPedidoDAO implements PedidoDAO {
 
@@ -58,6 +62,45 @@ public class JDBCPedidoDAO implements PedidoDAO {
 			e.printStackTrace();
 		}
 		return pedido;
+	}
+	
+	public List<ListaPedido> buscarPedido(String nome)  throws HamburgueriaException{
+		String comando = "SELECT pe.codpedido, pe.cliente_codcliente as codcliente, pe.funcionario_codfuncionario, "
+				+ "pe.data, pr.valor,"
+				+ " pr.nomeproduto, pr.descricao, pr.cancelamento, pr.anexo, pr.categoria, pr.codproduto,"
+				+ " pe.estagio_pedido_codestagio_pedido as estagio, pe.taxas_codtaxas as entrega, fc.nomefuncionario FROM pedido pe"
+				+ "inner join pedido_produto pp on pp.pedido_codpedido = pe.codpedido"
+				+ "inner join produto pr on pr.codproduto = pp.produto_codproduto"
+				+ "inner join funcionario fc on fc.codfuncionario = pe.cliente_codcliente";
+		if (!nome.equals("")) {
+			comando += "where pe.cliente_codcliente like '" + nome + "%'";
+		}
+		List<ListaPedido> list = new ArrayList<ListaPedido>();
+		ListaPedido produt = null;
+		try {
+			java.sql.Statement stmt = conexao.createStatement();
+			ResultSet rs = stmt.executeQuery(comando);
+			while (rs.next()) {
+				produt = new ListaPedido();
+				produt.setCancelado(rs.getString("cancelamento"));
+				produt.setCategoria(rs.getString("categoria"));
+				produt.setCodCliente(rs.getInt("codcliente"));
+				produt.setCodPedido(rs.getInt("codpedido"));
+				produt.setCodProduto(rs.getInt("codproduto"));
+				produt.setDataCompra(rs.getDate("data"));
+				produt.setDescricaoProduto(rs.getString("descricao"));
+				produt.setEntrega(rs.getInt("entrega"));
+				produt.setEstagio_produto(rs.getString("estagio"));
+				produt.setNomeProduto(rs.getString("nomeproduto"));
+				produt.setNomeUsuario(rs.getString("nome"));
+				produt.setValorProduto(rs.getFloat("valor"));
+				list.add(produt);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new HamburgueriaException();
+		}
+		return list;
 	}
 
 }
--- Hamburgueria/src/br/com/hamburgueria/objs/ListaPedido.java
+++ Hamburgueria/src/br/com/hamburgueria/objs/ListaPedido.java
@@ -1,5 +1,105 @@
 package br.com.hamburgueria.objs;
 
-public class ListaPedido {
+import java.util.Date;
 
+public class ListaPedido {
+	private int codPedido;
+	private int codProduto;
+	private int codCliente;
+	private Date dataCompra;
+	private float valorProduto;
+	private String nomeProduto;
+	private String descricaoProduto;
+	private String cancelado;
+	private String anexo;
+	private String categoria;
+	private String estagio_produto;
+	private int entrega;
+	private float valorTotal;
+	private String nomeUsuario;
+	
+	public int getCodPedido() {
+		return codPedido;
+	}
+	public void setCodPedido(int codPedido) {
+		this.codPedido = codPedido;
+	}
+	public int getCodProduto() {
+		return codProduto;
+	}
+	public void setCodProduto(int codProduto) {
+		this.codProduto = codProduto;
+	}
+	public int getCodCliente() {
+		return codCliente;
+	}
+	public void setCodCliente(int codCliente) {
+		this.codCliente = codCliente;
+	}
+	public Date getDataCompra() {
+		return dataCompra;
+	}
+	public void setDataCompra(Date dataCompra) {
+		this.dataCompra = dataCompra;
+	}
+	public float getValorProduto() {
+		return valorProduto;
+	}
+	public void setValorProduto(float valorProduto) {
+		this.valorProduto = valorProduto;
+	}
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public String getDescricaoProduto() {
+		return descricaoProduto;
+	}
+	public void setDescricaoProduto(String descricaoProduto) {
+		this.descricaoProduto = descricaoProduto;
+	}
+	public String getCancelado() {
+		return cancelado;
+	}
+	public void setCancelado(String cancelado) {
+		this.cancelado = cancelado;
+	}
+	public String getAnexo() {
+		return anexo;
+	}
+	public void setAnexo(String anexo) {
+		this.anexo = anexo;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getEstagio_produto() {
+		return estagio_produto;
+	}
+	public void setEstagio_produto(String estagio_produto) {
+		this.estagio_produto = estagio_produto;
+	}
+	public int getEntrega() {
+		return entrega;
+	}
+	public void setEntrega(int entrega) {
+		this.entrega = entrega;
+	}
+	public float getValorTotal() {
+		return valorTotal;
+	}
+	public void setValorTotal(float valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}	
 }
