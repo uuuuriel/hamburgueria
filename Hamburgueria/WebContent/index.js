af835d5d3a6304fab92ba6d2c58581a952416871
uuuuriel@gmail.com <uriel_augusto@joinville.sc.senai.br>
2016-04-29 21:29:29 -0300

    x

--- Hamburgueria/WebContent/index.js
+++ Hamburgueria/WebContent/index.js
@@ -1,21 +1,30 @@
-function carregar(pagina) {
-	$("#conteudo").load(pagina);
-}
-var base = "resources/uploads/";
-HM.sessao = function(data, back){
-	HM.ajax.post({
-		async: false,
-		url:"rest/Sessao/getSessao/"+ data,
-		success:function(succ){
-			back = succ.sessao;
-		},
-		error:function(err){
-			console.log(err);
-		}
+$(document).ready(function(){
+	window.onpopstate = function(event) {
+		console.dir(event);
+		$('#conteudo').load(event.state.url);
+	 }
+	$(".carregar").on("click",function(e){
+		e.preventDefault();
+//		var url = window.location.pathname.indexOf("/h/") > 0 ? "" : "h/";
+//		url += $(this).attr("title");
+		$("#conteudo").load($(this).attr("href"));
+		window.history.pushState({url:$(this).attr("href")}, $(this).attr("title"), $(this).attr("title"));
+	})
+	HM.sessao = function(data, back){
+		HM.ajax.post({
+			async: false,
+			url:"rest/Sessao/getSessao/"+ data,
+			success:function(succ){
+				back = succ.sessao;
+			},
+			error:function(err){
+				console.log(err);
+			}
+		});
+		return back;
+	};
+	$(document).ready(function() {
+		$('#conteudoIndex').load('resources/ofertadia.html');
+		$('#sugestaoCritica').load('resources/sugestaocritica.html');
 	});
-	return back;
-};
-$(document).ready(function() {
-	$('#conteudoIndex').load('resources/ofertadia.html');
-	$('#sugestaoCritica').load('resources/sugestaocritica.html');
 });
