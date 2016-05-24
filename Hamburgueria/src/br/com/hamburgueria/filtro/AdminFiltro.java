4bc3d0c7a2df520cd4f2fdd01cd7a7a29c4a0d76
uuuuriel@gmail.com <uriel_augusto@joinville.sc.senai.br>
2016-05-24 20:45:22 -0300

    x

--- Hamburgueria/src/br/com/hamburgueria/filtro/AdminFiltro.java
+++ Hamburgueria/src/br/com/hamburgueria/filtro/AdminFiltro.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class AdminFiltro {
+public class AdminFiltro implements Filter{
 
 	    public AdminFiltro() {
 	        // TODO Auto-generated constructor stub
