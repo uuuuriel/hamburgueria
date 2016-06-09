180a913b66db9ebd823339670e14dfe3d2c900db
uuuuriel@gmail.com <uriel_augusto@joinville.sc.senai.br>
2016-06-09 19:29:31 -0300

    x

---
+++ Hamburgueria/src/br/com/hamburgueria/auxilia/Permissao.java
@@ -0,0 +1,24 @@
+package br.com.hamburgueria.auxilia;
+
+import br.com.hamburgueria.exception.PermissaoException;
+
+public class Permissao {
+	
+	public void checkPermission(String key, int cod) throws PermissaoException{
+		switch (key) {
+		case "func":
+				if(cod != 1) throw new PermissaoException();
+			break;
+		case "adm":
+				if(cod != 1) throw new PermissaoException();
+			break;
+			
+		case "log":
+				if(cod != 4) throw new PermissaoException();
+			break;
+		default:
+			break;
+		}
+	}
+
+}
